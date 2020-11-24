e65fa431f26f26962d4164120a55191e1a4fa039
Paul <paul.lee@imagr.co>
2020-11-24 23:01:10 +1300

    Updated the detectron artifact

--- bentoml/frameworks/detectron.py
+++ bentoml/frameworks/detectron.py
@@ -1,15 +1,16 @@
 import os
-
 from bentoml.exceptions import MissingDependencyException
 from bentoml.service.artifacts import BentoServiceArtifact
 from bentoml.service.env import BentoServiceEnv
 
-class DetectronArtifact(BentoServiceArtifact):
+
+class DetectronModelArtifact(BentoServiceArtifact):
 
     def __init__(self, name):
-        super(DetectronArtifact, self).__init__(name)
-
+        super(DetectronModelArtifact, self).__init__(name)
+        self._file_name = name
         self._model = None
+        self._aug = None
 
     def set_dependencies(self, env: BentoServiceEnv):
         env.add_pip_packages(['torch', "detectron2"])
@@ -22,7 +23,7 @@ class DetectronArtifact(BentoServiceArtifact):
             import detectron2  # noqa # pylint: disable=unused-import
         except ImportError:
             raise MissingDependencyException(
-                "Detectron package is required to use DetectronArtifact"
+                "Detectron package is required to use DetectronModelArtifact"
             )
         self._model = detectron_model
         return self
@@ -32,24 +33,31 @@ class DetectronArtifact(BentoServiceArtifact):
             from detectron2.checkpoint import DetectionCheckpointer  # noqa # pylint: disable=unused-import
             from detectron2.modeling import META_ARCH_REGISTRY
             from detectron2.config import get_cfg
+            from detectron2.data import transforms as T
             import json
         except ImportError:
             raise MissingDependencyException(
                 "Detectron package is required to use DetectronArtifact"
             )
 
-        json_object = json.loads(path)
         cfg = get_cfg()
-        cfg.merge_from_file(json_object['cfg'])        
+        cfg.merge_from_file(f"{path}/{self._file_name}.yaml")        
         meta_arch = META_ARCH_REGISTRY.get(cfg.MODEL.META_ARCHITECTURE)
-        model = meta_arch(cfg)
-        model.eval()
-        device = "cuda:{}".format(json_object['device'])
-        model.to(device)
-        checkpointer = DetectionCheckpointer(model)
-        checkpointer.load(json_object['ckpt'])
-        return self.pack(model)
+        self._model = meta_arch(cfg)
+        self._model.eval()
 
+        device = os.environ.get('BENTOML_DEVICE')
+        if device == "GPU":
+            device = "cuda:0"
+        else:
+            device = "cpu"
+        self._model.to(device)
+        checkpointer = DetectionCheckpointer(self._model)
+        checkpointer.load(f"{path}/{self._file_name}.pth")
+        self._aug = T.ResizeShortestEdge(
+            [cfg.INPUT.MIN_SIZE_TEST, cfg.INPUT.MIN_SIZE_TEST], cfg.INPUT.MAX_SIZE_TEST
+        )
+        return self.pack(self._model)
 
     def get(self):
         return self._model
@@ -57,10 +65,18 @@ class DetectronArtifact(BentoServiceArtifact):
     def save(self, dst):
         try:
             from detectron2.checkpoint import DetectionCheckpointer  # noqa # pylint: disable=unused-import
+            from detectron2.config import get_cfg
         except ImportError:
             raise MissingDependencyException(
                 "Detectron package is required to use DetectronArtifact"
             )
-        checkpointer = DetectionCheckpointer(self._model, save_dir="output")
-        checkpointer.save("model")  # save to output/model_999.pth
+        os.makedirs(dst, exist_ok = True)
+        checkpointer = DetectionCheckpointer(self._model, save_dir=dst)
+        checkpointer.save(self._file_name)
+        cfg = get_cfg()
+        cfg.merge_from_file("input_model.yaml")
+        with open(os.path.join(dst, "model.yaml"), 'w', encoding='utf-8') as output_file:
+            output_file.write(cfg.dump())
+        
+
 
